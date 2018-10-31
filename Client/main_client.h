deaf991886fae039fcfd296c31d384c205a29db5
zhushuo <614251922@qq.com>
2018-10-31 21:09:57 +0800

    add some code

---
+++ Client/main_client.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main_client.h
+	> Author: zhushuo
+	> Mail: 
+	> Created Time: 2018年11月04日 星期二 21时16分42秒
+ ************************************************************************/
+
+#ifndef _MAIN_CLIENT_H
+#define _MAIN_CLIENT_H
+#include "client.h"
+#include "common.h"
+#include "client_func.h"
+#endif
