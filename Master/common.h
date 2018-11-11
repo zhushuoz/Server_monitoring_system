1b5a72226bb0d6d216f400d6ea2f6e83d9f345e7
zhushuo <614251922@qq.com>
2018-11-11 20:14:55 +0800

    add some code

---
+++ Master/common.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: common.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月10日 星期六 22时04分51秒
+ ************************************************************************/
+
+#ifndef _COMMON_H
+#define _COMMON_H
+#endif
+#include "master.h"
+
+char *get_conf_value(const char *pathname, const char *key_name);
+int write_Pi_log(char *piHealthLog, const char *format,...);
+int send_response(int sockfd, int req);
+int recv_response(int sockfd);
+int check_size(char *filename, int size, char *dir);
+int creat_listen(int n);
+int connect_socked(int port, char *host);
