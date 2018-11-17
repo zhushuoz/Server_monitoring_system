a6c332cbbb7bd7219fc0768b8ed58626851e2292
zhushuo <614251922@qq.com>
2018-11-17 23:18:16 +0800

    add some code

---
+++ Master/main_master.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: 1_master.h
+	> Author: zhushuo
+	> Mail: 
+	> Created Time: 2018年11月17日 星期六 11时38分59秒
+ ************************************************************************/
+
+#ifndef _1_MASTER_H
+#define _1_MASTER_H
+#include "master.h"
+#include "common.h"
+#include "master_func.h"
+#endif
